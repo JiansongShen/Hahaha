f8b3cedddcee0f3b17a8c72d927877676cc0b73f
napbad <napbad.sen@gmail.com>
2026-01-06 11:03:10 +0800

    optimize tests: auto grad test, basic training

--- examples/autograd/autograd_train.h
+++ examples/autograd/autograd_train.h
@@ -15,20 +15,21 @@
 // Contributors:
 // jiansongshen (jason.shen111@outlook.com) (https://github.com/jiansongshen)
 //
+#pragma once
 
 #include <chrono>
-#include <cmath>
 #include <random>
 #include <vector>
 
 #include "Tensor.h"
 #include "common/definitions.h"
+#include "ml/optimizer/SGDOptimizer.h"
+#include "utils/log/Logger.h"
 
 using hahaha::Tensor;
 using namespace hahaha::common;
 
-void nn_train_example() {
-
+inline void nn_train_example() {
     std::mt19937 engine;
 
     std::random_device randomDevice;
@@ -38,35 +39,60 @@ void nn_train_example() {
             std::chrono::steady_clock::now().time_since_epoch().count())};
     engine.seed(seed);
 
-    std::uniform_real_distribution<float> dist(0, 100);
+    std::uniform_real_distribution<float> dist(0, 1);
 
     constexpr int DataSize = 100;
-    constexpr int TrainLoop = 10;
+    constexpr int TrainLoop = 100;
 
     std::vector<f32> x;
     std::vector<f32> y;
-    x.reserve(DataSize);
-    y.reserve(DataSize);
+    x.resize(DataSize);
+    y.resize(DataSize);
 
     // y = 2 * x
     for (int i = 0; i < DataSize; ++i) {
-        auto val = dist(engine);
+        const auto val = dist(engine);
         x[i] = val;
         y[i] = val * 2;
     }
 
-    Tensor<f32> xTensor = Tensor<f32>::buildFromVector(x);
-    Tensor<f32> yTensor = Tensor<f32>::buildFromVector(y);
+    info("x: " + std::to_string(x[0]) + " y: " + std::to_string(y[0]));
+
+    const Tensor<f32> xTensor = Tensor<f32>::buildFromVector(x);
+    const Tensor<f32> yTensor = Tensor<f32>::buildFromVector(y);
+    Tensor<f32> bias(static_cast<f32>(0));
     Tensor<f32> w(1);
+    w.setRequiresGrad(true);
+    bias.setRequiresGrad(true);
     // y = w * x
     Tensor<f32> loss(1);
+    loss.setRequiresGrad(true);
+    hahaha::ml::SGDOptimizer<f32> optimizer({w, bias}, 0.005);
 
     for (int i = 0; i < TrainLoop; ++i) {
-        loss.clearGrad();
+        optimizer.zeroGrad();
         loss.clear();
-        for (int i = 0; i < DataSize; ++i) {
-            loss = loss + (yTensor - w * xTensor).matmul(yTensor - w * xTensor);
-        }
+
+        info("===================epoch " + std::to_string(i)
+             + "===================");
+        info("w: " + std::to_string(w.at({})));
+        info("bias: " + std::to_string(bias.at({})));
+
+        Tensor<f32> yPredict = w * xTensor + bias;
+
+        loss = (yTensor - yPredict)
+                   .reshape({1, 100})
+                   .matmul((yTensor - yPredict).reshape({100, 1}));
+
         loss.backward();
+
+        info("grad of    w: " + std::to_string(w.grad()->at({})));
+        info("grad of bias: " + std::to_string(bias.grad()->at({})));
+        info("grad of loss: " + std::to_string(loss.grad()->at({0, 0})));
+
+        optimizer.step();
+
+        info("loss: " + std::to_string(loss.at({0, 0})));
+        info("");
     }
 }
