4dca432c922bf5869214cc11760d7e3f8ec12e33
napbad <napbad.sen@gmail.com>
2026-01-07 16:02:28 +0800

    update ci coverage gate

--- dev/coverage.sh
+++ dev/coverage.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-###
- # @Author: napbad napbad.sen@gmail.com
- # @Date: 2026-01-07 11:07:08
- # @LastEditors: napbad napbad.sen@gmail.com
- # @LastEditTime: 2026-01-07 13:45:27
- # @FilePath: /Hahaha/dev/coverage.sh
- # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-###
 set -euo pipefail
 
 # Usage:
--- tests/core/ml/model/LinearRegressionTest.cpp
+++ tests/core/ml/model/LinearRegressionTest.cpp
@@ -1,12 +1,4 @@
-/*
- * @Author: napbad napbad.sen@gmail.com
- * @Date: 2026-01-07 11:22:15
- * @LastEditors: napbad napbad.sen@gmail.com
- * @LastEditTime: 2026-01-07 14:55:35
- * @FilePath: /Hahaha/tests/core/ml/model/LinearRegressionTest.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
- * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
 //  Copyright (c) 2026 Contributors of hahaha(https://github.com/Napbad/Hahaha)
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
